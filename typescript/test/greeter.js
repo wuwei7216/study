211fea471d5ce0c4d0ae871a6ebfde215b709e10
wuwei <wuwei@wezhuiyi.com>
2022-02-23 18:04:41 +0800

    typescript 01

---
+++ typescript/test/greeter.js
@@ -0,0 +1,19 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    /**
+     * name
+     */
+    Student.prototype.name = function () {
+    };
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = new Student("Jane", "M.", "User");
+document.body.innerHTML = greeter(user);
---
+++ typescript/test/greeter.ts
@@ -0,0 +1,25 @@
+class Student {
+  fullName: string;
+  constructor(public firstName, public middleInitial, public lastName) {
+      this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+ /**
+  * name
+  */
+ public name() {
+   
+ }
+}
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person : Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane", "M.", "User");
+
+document.body.innerHTML = greeter(user);
