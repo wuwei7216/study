5018e8df1dc3ddf96d8718cc0494d23deba54899
wuwei <wuwei@wezhuiyi.com>
2022-02-25 18:54:27 +0800

    typescript 03 class

---
+++ typescript/doc/class.ts
@@ -0,0 +1,45 @@
+// 类
+class Greeter {
+  greeting: string;
+  constructor(message: string) {
+      this.greeting = message;
+  }
+  greet() {
+      return "Hello, " + this.greeting;
+  }
+}
+
+let greeter = new Greeter("world");
+
+// 继承
+class Animal {
+  name: string;
+  constructor(theName: string) { this.name = theName; }
+  move(distanceInMeters: number = 0) {
+      console.log(`${this.name} moved ${distanceInMeters}m.`);
+  }
+}
+
+class Snake extends Animal {
+  constructor(name: string) { super(name); }
+  move(distanceInMeters = 5) {
+      console.log("Slithering...");
+      super.move(distanceInMeters);
+  }
+}
+
+class Horse extends Animal {
+  constructor(name: string) { super(name); }
+  move(distanceInMeters = 45) {
+      console.log("Galloping...");
+      super.move(distanceInMeters);
+  }
+}
+
+let sam = new Snake("Sammy the Python");
+let tom: Animal = new Horse("Tommy the Palomino");
+
+sam.move();
+tom.move(34);
+
+
