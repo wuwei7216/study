d66ef15dbda4f0a2f6ce8ab3ab00924eb848d811
wuwei <wuwei@wezhuiyi.com>
2022-03-07 18:34:55 +0800

    装饰器02

--- typescript/doc/decorator.js
+++ typescript/doc/decorator.js
@@ -103,4 +103,55 @@ var decorator;
             descriptor.enumerable = value;
         };
     }
+    // 访问器装饰器
+    // 问器装饰器声明在一个访问器的声明之前（紧靠着访问器声明）。 访问器装饰器应用于访问器的 属性描述符并且可以用来监视，修改或替换一个访问器的定义。 访问器装饰器不能用在声明文件中（.d.ts），或者任何外部上下文（比如 declare的类）里。
+    //  TypeScript不允许同时装饰一个成员的get和set访问器。取而代之的是，一个成员的所有装饰的必须应用在文档顺序的第一个访问器上。这是因为，在装饰器应用于一个属性描述符时，它联合了get和set访问器，而不是分开声明的。
+    // 访问器装饰器表达式会在运行时当作函数被调用，传入下列3个参数：
+    // 对于静态成员来说是类的构造函数，对于实例成员是类的原型对象。
+    // 成员的名字。
+    // 成员的属性描述符。
+    var Point = /** @class */ (function () {
+        function Point(x, y, name) {
+            this._x = x;
+            this._y = y;
+            this.name = name;
+        }
+        Point.prototype.method = function () { };
+        Object.defineProperty(Point.prototype, "x", {
+            get: function () { return this._x; },
+            enumerable: false,
+            configurable: true
+        });
+        Object.defineProperty(Point.prototype, "y", {
+            get: function () { return this._y; },
+            enumerable: false,
+            configurable: true
+        });
+        __decorate([
+            configurable(false)
+        ], Point.prototype, "method", null);
+        __decorate([
+            configurable(false)
+        ], Point.prototype, "x", null);
+        __decorate([
+            configurable(false)
+        ], Point.prototype, "y", null);
+        return Point;
+    }());
+    function configurable(value) {
+        return function (target, propertyKey, descriptor) {
+            descriptor.configurable = value;
+        };
+    }
+    // 属性装饰器
+    // 属性装饰器表达式会在运行时当作函数被调用，传入下列2个参数：
+    // 对于静态成员来说是类的构造函数，对于实例成员是类的原型对象。
+    // 成员的名字。
+    // 参数装饰器
+    // 参数装饰器声明在一个参数声明之前（紧靠着参数声明）。 参数装饰器应用于类构造函数或方法声明。 参数装饰器不能用在声明文件（.d.ts），重载或其它外部上下文（比如 declare的类）里。
+    // 参数装饰器表达式会在运行时当作函数被调用，传入下列3个参数：
+    // 对于静态成员来说是类的构造函数，对于实例成员是类的原型对象。
+    // 成员的名字。
+    // 参数在函数参数列表中的索引。
+    // 注意  参数装饰器只能用来监视一个方法的参数是否被传入。
 })(decorator || (decorator = {}));
