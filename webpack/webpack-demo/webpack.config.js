fef28f04e39bf5e15415cc4f9925fdf6ac7b09b9
wuwei <wuwei@wezhuiyi.com>
2022-04-06 14:04:43 +0800

    webpack 管理输出

--- webpack/webpack-demo/webpack.config.js
+++ webpack/webpack-demo/webpack.config.js
@@ -1,10 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// import { Configuration } from 'webpack' 放开后可以看到webpack配置提示
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+/**
+ * @type {Configuration}
+ */
+ const config = {
   mode: 'development',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '管理输出',
+    }),
+  ],
 };
+
+module.exports = config
