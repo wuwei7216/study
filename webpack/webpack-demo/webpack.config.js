eeb4c8cbede177beda7fa865cd5debf6e4ad79b1
wuwei <wuwei@wezhuiyi.com>
2022-06-15 16:05:52 +0800

    webpack 官网 模块热替换

--- webpack/webpack-demo/webpack.config.js
+++ webpack/webpack-demo/webpack.config.js
@@ -1,30 +1,40 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// import { Configuration } from 'webpack' 放开后可以看到webpack配置提示
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const webpack = require('webpack');
+// import { Configuration } from 'webpack' // 放开后可以看到webpack配置提示
 
 /**
  * @type {Configuration}
  */
- const config = {
+const config = {
   mode: 'development',
   entry: {
-    index: './src/index.js',
-    print: './src/print.js'
+    index: './src/index.js'
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
+  },
   devServer: {
     static: './dist',
+    hot: true
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'development',
     }),
+    new webpack.HotModuleReplacementPlugin()
   ],
   devtool: 'inline-source-map',
 };
