e00b6944e5030157df09ca2de1fe566fa5702ba9
A0251530_wuwei <wuwei@gmail.com>
2022-10-26 10:52:28 +0800

    vue3 jsx

--- vue/vue3-all/geek-admin/src/pages/about.vue
+++ vue/vue3-all/geek-admin/src/pages/about.vue
@@ -1,4 +1,13 @@
 
-<template>
-    <h1>这是关于页面</h1>
-</template>
+ <template>
+    <div>
+        <div>关于页面</div>
+        <Heading :level="3">hello geekbang1</Heading>
+        <Todo></Todo>
+    </div>
+  </template>
+  
+  <script setup>
+  import Heading from '../components/Heading.jsx'
+  import Todo from '../components/Todo.jsx'
+  </script>
